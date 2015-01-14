a79b3dac33b579bba90702c5c3407e3fbc846361
Brice DUCARDONNOY <brice@ducardonnoy.com>
2015-01-14 22:18:04 +0100

    Initial drop with a welcome page with GWT-Bootstrap component, logs and i18n

---
+++ SewGui/src/main/java/com/briceducardonnoy/sewgui/client/place/NameTokens.java
@@ -0,0 +1,9 @@
+package com.briceducardonnoy.sewgui.client.place;
+
+public class NameTokens {
+    public static final String home = "/home";
+
+    public static String getHome() {
+        return home;
+    }
+}
---
+++ SewGui/src/main/java/com/briceducardonnoy/sewgui/server/guice/ServerModule.java
@@ -0,0 +1,9 @@
+package com.briceducardonnoy.sewgui.server.guice;
+
+import com.gwtplatform.dispatch.rpc.server.guice.HandlerModule;
+
+public class ServerModule extends HandlerModule {
+    @Override
+    protected void configureHandlers() {
+    }
+}
