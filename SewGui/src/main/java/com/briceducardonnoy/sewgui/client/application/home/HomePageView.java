deb7b3a70fdc5ad462358e8ea54ace7772ef19c1
Brice DUCARDONNOY <brice@ducardonnoy.com>
2015-01-18 16:20:06 +0100

    Dummy commit

--- SewGui/src/main/java/com/briceducardonnoy/sewgui/client/application/home/HomePageView.java
+++ SewGui/src/main/java/com/briceducardonnoy/sewgui/client/application/home/HomePageView.java
@@ -2,6 +2,9 @@ package com.briceducardonnoy.sewgui.client.application.home;
 
 import javax.inject.Inject;
 
+import com.allen_sauer.gwt.log.client.Log;
+import com.briceducardonnoy.sewgui.client.lang.Translate;
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
@@ -9,9 +12,12 @@ import com.gwtplatform.mvp.client.ViewImpl;
 class HomePageView extends ViewImpl implements HomePagePresenter.MyView {
 	interface Binder extends UiBinder<Widget, HomePageView> {
 	}
+	
+	private final Translate translate = GWT.create(Translate.class);
 
 	@Inject
 	HomePageView(Binder uiBinder) {
 		initWidget(uiBinder.createAndBindUi(this));
+		Log.info(translate.Bonjour());
 	}
 }
