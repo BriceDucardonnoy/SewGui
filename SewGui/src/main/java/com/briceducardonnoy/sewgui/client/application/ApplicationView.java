4b9c1971be4645892d153b8b1cc4e0154a0562b6
Brice DUCARDONNOY <brice@ducardonnoy.com>
2015-01-31 20:58:08 +0100

    Update copyright and index.html

--- SewGui/src/main/java/com/briceducardonnoy/sewgui/client/application/ApplicationView.java
+++ SewGui/src/main/java/com/briceducardonnoy/sewgui/client/application/ApplicationView.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2015 © Brice DUCARDONNOY
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included 
+ * 	in all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express 
+ * 	or implied, including but not limited to the warranties of merchantability, 
+ * 	fitness for a particular purpose and noninfringement.
+ * 
+ * In no event shall the authors or copyright holders be liable for any claim, 
+ * damages or other liability, whether in an action of contract, tort or otherwise, 
+ * arising from, out of or in connection with the software or the use or other 
+ * dealings in the Software.
+ */
 package com.briceducardonnoy.sewgui.client.application;
 
 import org.gwtbootstrap3.client.ui.AnchorListItem;
@@ -11,6 +31,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -20,77 +41,82 @@ import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 
 class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
-    
+
 	interface Binder extends UiBinder<Widget, ApplicationView> {
-    }
-    
-    private final Translate translate = GWT.create(Translate.class);
+	}
 
-    @Inject PlaceManager placeManager;
-    
-    @UiField SimplePanel main;
-    @UiField NavbarBrand brand;
-    @UiField AnchorListItem status;
-    @UiField AnchorListItem network;
-    @UiField AnchorListItem stream;
-    
-    private PlaceRequest statusGo;
-    private PlaceRequest networkGo;
-    private PlaceRequest streamGo;
+	private final Translate translate = GWT.create(Translate.class);
 
-    @Inject
-    ApplicationView(Binder uiBinder) {
-        initWidget(uiBinder.createAndBindUi(this));
-        Log.info(translate.Bonjour() + " from appsView");
-        
-        statusGo = new PlaceRequest.Builder().nameToken(NameTokens.getStatus()).build();
-        networkGo = new PlaceRequest.Builder().nameToken(NameTokens.getNetwork()).build();
-        streamGo = new PlaceRequest.Builder().nameToken(NameTokens.getStream()).build();
-    }
+	@Inject PlaceManager placeManager;
 
-    @Override
-    public void setInSlot(Object slot, IsWidget content) {
-        if (slot == ApplicationPresenter.SLOT_SetMainContent) {
-            main.setWidget(content);
-        } else {
-            super.setInSlot(slot, content);
-        }
-    }
-    
-    @UiHandler("brand")
-    void onBrandSelected(ClickEvent event) {
-    	Log.info("Brand clicked");
-    	placeManager.revealDefaultPlace();
-    	status.setActive(true);// TODO BDY: optimise it
-    	network.setActive(false);
-    	stream.setActive(false);
-    }
-    
-    @UiHandler("status")
-    void onStatusSelected(ClickEvent event) {
-    	Log.info("Status clicked");
-    	placeManager.revealPlace(statusGo);
-    	status.setActive(true);
-    	network.setActive(false);
-    	stream.setActive(false);
-    }
-    
-    @UiHandler("network")
-    void onNetworkSelected(ClickEvent event) {
-    	Log.info("Network clicked");
-    	placeManager.revealPlace(networkGo);
-    	status.setActive(false);
-    	network.setActive(true);
-    	stream.setActive(false);
-    }
-    
-    @UiHandler("stream")
-    void onStreamSelected(ClickEvent event) {
-    	Log.info("Stream clicked");
-    	placeManager.revealPlace(streamGo);
-    	status.setActive(false);
-    	network.setActive(false);
-    	stream.setActive(true);
-    }
-    
+	@UiField SimplePanel main;
+	@UiField NavbarBrand brand;
+	@UiField AnchorListItem status;
+	@UiField AnchorListItem network;
+	@UiField AnchorListItem stream;
+
+	private PlaceRequest statusGo;
+	private PlaceRequest networkGo;
+	private PlaceRequest streamGo;
+
+	@Inject
+	ApplicationView(Binder uiBinder) {
+		initWidget(uiBinder.createAndBindUi(this));
+		Log.info(translate.Bonjour() + " from appsView");
+
+		statusGo = new PlaceRequest.Builder().nameToken(NameTokens.getStatus()).build();
+		networkGo = new PlaceRequest.Builder().nameToken(NameTokens.getNetwork()).build();
+		streamGo = new PlaceRequest.Builder().nameToken(NameTokens.getStream()).build();
+	}
+
+	@Override
+	public void setInSlot(Object slot, IsWidget content) {
+		if (slot == ApplicationPresenter.SLOT_SetMainContent) {
+			main.setWidget(content);
+		} else {
+			super.setInSlot(slot, content);
+		}
+	}
+
+	@UiHandler("brand")
+	void onBrandSelected(ClickEvent event) {
+		Log.info("Brand clicked");
+		placeManager.revealDefaultPlace();
+		status.setActive(true);// TODO BDY: optimise it
+		network.setActive(false);
+		stream.setActive(false);
+	}
+
+	@UiHandler("status")
+	void onStatusSelected(ClickEvent event) {
+		Log.info("Status clicked");
+		placeManager.revealPlace(statusGo);
+		status.setActive(true);
+		network.setActive(false);
+		stream.setActive(false);
+	}
+
+	@UiHandler("network")
+	void onNetworkSelected(ClickEvent event) {
+		Log.info("Network clicked");
+		placeManager.revealPlace(networkGo);
+		status.setActive(false);
+		network.setActive(true);
+		stream.setActive(false);
+	}
+
+	@UiHandler("stream")
+	void onStreamSelected(ClickEvent event) {
+		Log.info("Stream clicked");
+		placeManager.revealPlace(streamGo);
+		status.setActive(false);
+		network.setActive(false);
+		stream.setActive(true);
+	}
+
+	@UiHandler("reload")
+	void onReload(ClickEvent event) {
+		Window.Location.reload();
+	}
+
 }
