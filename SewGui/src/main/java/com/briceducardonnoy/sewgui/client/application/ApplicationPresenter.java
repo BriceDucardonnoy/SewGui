f324c4b501fdbcf7d91c060feefb277261b460ff
Brice DUCARDONNOY <brice@ducardonnoy.com>
2015-01-31 20:58:33 +0100

    Succeed to get remote log from both web and phonegap emulation

--- SewGui/src/main/java/com/briceducardonnoy/sewgui/client/application/ApplicationPresenter.java
+++ SewGui/src/main/java/com/briceducardonnoy/sewgui/client/application/ApplicationPresenter.java
@@ -1,9 +1,19 @@
 package com.briceducardonnoy.sewgui.client.application;
 
-import javax.inject.Inject;
+import java.util.logging.Logger;
 
+import com.allen_sauer.gwt.log.client.Log;
+import com.briceducardonnoy.sewgui.client.wrappers.BluetoothSerialImpl;
+import com.google.gwt.core.client.Callback;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
+import com.googlecode.gwtphonegap.client.PhoneGap;
+import com.googlecode.gwtphonegap.client.PhoneGapAvailableEvent;
+import com.googlecode.gwtphonegap.client.PhoneGapAvailableHandler;
+import com.googlecode.gwtphonegap.client.PhoneGapTimeoutEvent;
+import com.googlecode.gwtphonegap.client.PhoneGapTimeoutHandler;
 import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.ContentSlot;
@@ -12,18 +22,54 @@ import com.gwtplatform.mvp.client.proxy.Proxy;
 import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
 
 public class ApplicationPresenter extends Presenter<ApplicationPresenter.MyView, ApplicationPresenter.MyProxy> {
-    interface MyView extends View {
-    }
+	interface MyView extends View {
+	}
 
-    @ContentSlot
-    public static final Type<RevealContentHandler<?>> SLOT_SetMainContent = new Type<>();
+	@ContentSlot
+	public static final Type<RevealContentHandler<?>> SLOT_SetMainContent = new Type<>();
 
-    @ProxyStandard
-    interface MyProxy extends Proxy<ApplicationPresenter> {
-    }
-
-    @Inject
-    ApplicationPresenter(EventBus eventBus, MyView view, MyProxy proxy) {
-        super(eventBus, view, proxy, RevealType.Root);
-    }
+	@ProxyStandard
+	interface MyProxy extends Proxy<ApplicationPresenter> {
+	}
+	// TODO BDY: set phonegap as an injectable object
+	@Inject
+	ApplicationPresenter(EventBus eventBus, MyView view, MyProxy proxy) {
+		super(eventBus, view, proxy, RevealType.Root);
+		final PhoneGap phoneGap = GWT.create(PhoneGap.class);
+		phoneGap.addHandler(new PhoneGapAvailableHandler() {
+			@Override
+			public void onPhoneGapAvailable(PhoneGapAvailableEvent event) {
+				//start your app - phonegap is ready
+				Log.info("PhoneGap available");
+			}
+		});
+		phoneGap.addHandler(new PhoneGapTimeoutHandler() {
+			@Override
+			public void onPhoneGapTimeout(PhoneGapTimeoutEvent event) {
+				//can not start phonegap - something is for with your setup
+				Log.error("PhoneGap unavailable");
+			}
+		});
+		phoneGap.initializePhoneGap();
+		phoneGap.getLog().setRemoteLogServiceUrl("http://192.168.1.46:8080/gwt-log");
+		final Logger logger = Logger.getLogger("SewGui");
+		BluetoothSerialImpl btImpl = new BluetoothSerialImpl();
+		btImpl.initialize();
+		logger.severe("patate");
+		phoneGap.loadPlugin("bluetoothSerialImpl", btImpl);
+		btImpl.isEnabled(new Callback<Boolean, String>() {
+			
+			@Override
+			public void onSuccess(Boolean result) {
+				Log.info("Success result is " + result);
+				logger.info("Success result is " + result);
+			}
+			
+			@Override
+			public void onFailure(String reason) {
+				Log.error("Failure reason is " + reason);
+				logger.severe("Failure reason is " + reason);
+			}
+		});
+	}
 }
---
+++ SewGui/src/main/java/com/briceducardonnoy/sewgui/client/wrappers/BluetoothSerialImpl.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2015 © Brice DUCARDONNOY
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included 
+ * 	in all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express 
+ * 	or implied, including but not limited to the warranties of merchantability, 
+ * 	fitness for a particular purpose and noninfringement.
+ * 
+ * In no event shall the authors or copyright holders be liable for any claim, 
+ * damages or other liability, whether in an action of contract, tort or otherwise, 
+ * arising from, out of or in connection with the software or the use or other 
+ * dealings in the Software.
+ */
+package com.briceducardonnoy.sewgui.client.wrappers;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.core.client.Callback;
+
+public class BluetoothSerialImpl implements BluetoothPlugin {
+
+	@Override
+	public void initialize() {
+		Log.info("Initialize BT");
+	}
+
+	@Override
+	public void isEnabled(Callback<Boolean, String> callback) {
+		isEnabledNative(callback);
+	}
+	
+	private native void isEnabledNative(Callback<Boolean, String> callback) /*-{
+		var success = function() { alert("Success"); };
+		var failure = function() { alert("Failed"); };
+//		var success = function() {
+//	        callback.@com.google.gwt.core.client.Callback::onSuccess(Ljava/lang/Boolean;)(true);
+//    	};
+//    	var failure = function() {
+//    		//callback.@com.google.gwt.core.client.Callback::onSuccess(Ljava/lang/Boolean;)(false);
+//    		callback.@com.google.gwt.core.client.Callback::onFailure(Ljava/lang/String;)("Bluetooth is disabled patate");
+//    	};
+//		cordova.exec($entry(success), $entry(failure), "BluetoothSerial", "isEnabled", []);
+	}-*/;
+
+}
